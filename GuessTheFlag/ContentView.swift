428663dd2aca5db7affe8f5c48e44dffafdac803
Karlyn Sanders <karlynwright@gmail.com>
2023-08-23 11:20:55 -0600

    score counter working

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,7 +12,7 @@ import SwiftUI
         @State private var scoreTitle = ""
         @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
         @State private var correctAnswer = Int.random(in: 0...2)
-        
+        @State private var userScore = 0
         
         
         var body: some View {
@@ -41,14 +41,15 @@ import SwiftUI
             .alert(scoreTitle, isPresented: $showingScore) {
                 Button("Continue", action: askQuestion)
             } message: {
-                Text("Your score is ???")
+                Text("Your score is \(userScore)")
             }
         }
         func flagTapped(_ number: Int) {
             if number == correctAnswer{
-                scoreTitle = "Correct"
+                scoreTitle = "Correct! \(countries[number])"
+                userScore+=1
             } else {
-                scoreTitle = "Wrong"
+                scoreTitle = "Wrong. That's the flag of \(countries[number])"
             }
             showingScore = true
             
