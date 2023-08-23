9190d995c90b565ec3ea92c425ae4a9070dcdc82
Karlyn Sanders <karlynwright@gmail.com>
2023-08-23 11:10:25 -0600

    added view modifier

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,62 +7,83 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var showingScore = false
-    @State private var scoreTitle = ""
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
-   
-    
-    var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
-        VStack(spacing:30){
-            VStack {
-                Text("Tap the flag of")
-                    .foregroundColor(.white)
-                    .font(.subheadline.weight(.heavy))
-                Text(countries[correctAnswer])
-                    .foregroundColor(.white)
-                    .font(.largeTitle.weight(.semibold))
-            }
+    struct ContentView: View {
+        @State private var showingScore = false
+        @State private var scoreTitle = ""
+        @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+        @State private var correctAnswer = Int.random(in: 0...2)
         
-            ForEach(0..<3) {number in
-                Button {
-                        flagTapped(number)
-                } label: {
-                    Image(countries[number])
-                        .renderingMode(.original)
-                        .clipShape(Capsule())
-                        .shadow(radius: 5)
+        
+        
+        var body: some View {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                VStack(spacing:30){
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundColor(.white)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .foregroundColor(.white)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) {number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            ImageView(image: countries[number])
+                        }
                     }
                 }
             }
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is ???")
+            }
         }
-        .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
-        } message: {
-            Text("Your score is ???")
+        func flagTapped(_ number: Int) {
+            if number == correctAnswer{
+                scoreTitle = "Correct"
+            } else {
+                scoreTitle = "Wrong"
+            }
+            showingScore = true
+            
+        }
+        func askQuestion() {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
         }
     }
-    func flagTapped(_ number: Int) {
-        if number == correctAnswer{
-            scoreTitle = "Correct"
-        } else {
-            scoreTitle = "Wrong"
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
-        showingScore = true
-        
-        }
-    func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+    }
+
+
+struct ImageView: View {
+    
+    var image: String
+    
+    var body: some View {
+        Image(image)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct flagViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline.weight(.heavy))
+            .foregroundColor(.blue)
     }
 }
